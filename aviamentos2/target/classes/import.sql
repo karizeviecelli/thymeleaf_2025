7be22b68404ed2abf179b93abcd10efd15fbf5c7
Karize Viecelli <46922770+karizeviecelli@users.noreply.github.com>
2025-10-21 16:13:59 -0300

    Crud 2

---
+++ aviamentos2/target/classes/import.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO TB_USUARIOS (CRIADO_EM, EMAIL, NOME, ROLE, SENHA) VALUES (now(),'ricardoreishg@gmail.com', 'ricardo', 'admin', 'cachorro124');
+INSERT INTO TB_USUARIOS (CRIADO_EM, EMAIL, NOME, ROLE, SENHA) VALUES (now(),'maria@maria', 'maria', 'user', '123');
+
